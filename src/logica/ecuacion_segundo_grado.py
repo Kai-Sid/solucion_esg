dcd5d4fc17b72faed308fe782542430ab33aefcf
Kai-Sid <72875432@continental.edu.pe>
2025-04-30 11:48:19 -0500

    Se implementó cálculo de raíces complejas

--- src/logica/ecuacion_segundo_grado.py
+++ src/logica/ecuacion_segundo_grado.py
@@ -45,11 +45,13 @@ class EcuacionSegundoGrado:
             raise ValueError("c debe ser un número")
 
     def solucionESG(self):
-        return None, None
-
-    def solucionESG(self):
         d = math.pow(self.b, 2) - 4 * self.a * self.c
         if d >= 0:
             r1 = (-self.b + math.sqrt(d)) / (2 * self.a)
-            r2 = (-self.b - math.sqrt(d)) / (2 * self.a)
+        r2 = (-self.b - math.sqrt(d)) / (2 * self.a)
+        else:
+        parteReal = -self.b / (2 * self.a)
+        parteImaginaria = math.sqrt(math.fabs(d)) / (2 * self.a)
+        r1 = complex(parteReal, parteImaginaria)
+        r2 = complex(parteReal, -parteImaginaria)
         return r1, r2
